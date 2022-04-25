b367993367727b5db0673e34ebfc050a22d366c6
Hitesh <hitesh@pop-os.localdomain>
2022-04-25 19:03:59 +0530

    :construction: updated: app engine for hbs

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require("express");
-const exphbs = require("express-handlebars");
+const { engine } = require("express-handlebars");
 const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+
+require("dotenv").config();
 
 const app = express();
-
 const port = process.env.PORT || 3000;
+
+// Parsing MiddleWare
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Parse application/json
+app.use(bodyParser.json());
+
+// Static files
+app.use(express.static("public"));
+
+// Templating Engine
+app.engine("hbs", engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+// Routes
+app.get("", (req, res) => {
+    res.render("home");
+});
+
 app.listen(port, () => {
     console.log(`server running on ${port}`);
 });
